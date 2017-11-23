6725e5f59bf8a996706bf5c7b82d34d768bd2001
MiguelAbadia <33655327+MiguelAbadia@users.noreply.github.com>
2017-11-23 11:55:44 +0100

    Add files via upload

---
+++ nif.py
@@ -0,0 +1,52 @@
+def nif():
+    x=input("Dime un numero")
+    if x<1000000000 and x>9999999:
+        resto=x%23
+    if resto==0:
+        print "La letra asignada es T"
+    if resto==1:
+        print "La letra asignada es R"
+    if resto==2:
+        print "La letra asignada es W"
+    if resto==3:
+        print "La letra asignada es A"
+    if resto==4:
+        print "La letra asignada es G"
+    if resto==5:
+        print "La letra asignada es M"
+    if resto==6:
+        print "La letra asignada es Y"
+    if resto==7:
+        print "La letra asignada es F"
+    if resto==8:
+        print "La letra asignada es P"
+    if resto==9:
+        print "La letra asignada es D"
+    if resto==10:
+        print "La letra asignada es X"
+    if resto==11:
+        print "La letra asignada es B"
+    if resto==12:
+        print "La letra asignada es N"
+    if resto==13:
+        print "La letra asignada es J"
+    if resto==14:
+        print "La letra asignada es Z"
+    if resto==15:
+        print "La letra asignada es S"
+    if resto==16:
+        print "La letra asignada es Q"
+    if resto==17:
+        print "La letra asignada es V"
+    if resto==18:
+        print "La letra asignada es H"
+    if resto==19:
+        print "La letra asignada es L"
+    if resto==20:
+        print "La letra asignada es C"
+    if resto==21:
+        print "La letra asignada es K"
+    if resto==22:
+        print "La letra asignada es E"  
+
+nif()
