b12d5379fdfe7568ddee3a6ac6f4e0f868ef82a2
MiguelAbadia <33655327+MiguelAbadia@users.noreply.github.com>
2017-11-23 11:52:56 +0100

    Add files via upload

---
+++ HolaMundo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+    int salir;
+    std::cout<<"Hola Miguel";    
+    
+    std::cout<<"Toca cualquier teclas para salir";
+    std::cin>>salir;
+    return 0;
+
+
+
+
+
+
+
+
+}
